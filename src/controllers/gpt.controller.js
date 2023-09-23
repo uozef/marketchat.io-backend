5a332462b41702faf9bc34e9d675c4efe7389b32
mohammad <mm4heidary@gmail.com>
2023-09-23 13:51:01 +0330

    fix bug

--- src/controllers/gpt.controller.js
+++ src/controllers/gpt.controller.js
@@ -6,7 +6,7 @@ exports.askChatGPT = async (req, res) => {
             return res.status(400).json({ error: "Prompt is required" });
         }
         const prompt = req.body.prompt;
-        const response = await chatgptService.getChatGPTResponse(prompt);
+        const response = await chatgptService.getGPTResponse(prompt);
         res.json(response);
     } catch (error) {
         res.status(500).json({ error: error.message });
