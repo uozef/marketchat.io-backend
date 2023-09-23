0581b4fb8f572ddc9942f4875d2a2f043032e44c
mohammad <mm4heidary@gmail.com>
2023-09-23 13:32:14 +0330

    update service

--- src/services/gpt.service.js
+++ src/services/gpt.service.js
@@ -1,23 +1,21 @@
 const OPENAI_API_BASE_URL = 'https://api.openai.com/v1/engines';
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-const CHATGPT_MODEL = process.env.CHATGPT_MODEL || 'davinci'; // default to 'davinci' if no model is specified in .env
 const axios = require('axios');
 exports.getChatGPTResponse = async (prompt) => {
     const headers = {
         'Authorization': `Bearer ${OPENAI_API_KEY}`,
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'User-Agent': 'OpenAI-NodeJS-Client'
     };
 
     const data = {
         prompt: prompt,
-        max_tokens: 150
+        max_tokens: 150 // You can adjust this as needed
     };
-
     try {
-        const response = await axios.post(OPENAI_API_BASE_URL, data, { headers: headers });
+        const response = await axios.post(OPENAI_API_URL, data, { headers: headers });
         return response.data.choices[0].text.trim();
     } catch (error) {
-        console.log(error);
-        throw new Error(`OpenAI API Error: ${error.response ? error.response.data.error : error.message}`);
+        console.error('Error calling OpenAI API:', error);
+        throw error;
     }
 };
